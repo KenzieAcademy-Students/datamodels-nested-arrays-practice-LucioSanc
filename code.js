09e930e13a91be422b6258c7618be9b4561ddf97
kenzieadmin <kenzieadmin@users.noreply.github.com>
2022-07-14 02:51:48 +0000

    deploy: 74ae752a1557b727b551e2dde456a892bea5f74e

--- code.js
+++ code.js
@@ -1,19 +1,6 @@
-const map = [
-  "WWWWWWWWWWWWWWWWWWWWW",
-  "W   W     W     W   W",
-  "W W W WWW WWWWW W WWW",
-  "W W W  W      W W   W",
-  "W WWWWWWW W WWW W W W",
-  "W         W     W W W",
-  "W WWW WWWWW WWWWW W W",
-  "W W   W   W W     W W",
-  "W WWWWW W W W WWW W F",
-  "S     W W W W W W WWW",
-  "WWWWW W W W W W W W W",
-  "W     W W W   W W W W",
-  "W WWWWWWW WWWWW W W W",
-  "W       W       W   W",
-  "WWWWWWWWWWWWWWWWWWWWW"
-];
+const mazeView = document.querySelector("main");
+generateMaze(mazeView);
 
-// Your Code Here.
+const startingCell = document.querySelector(".start");
+const playerView = renderPlayer(startingCell);
+
